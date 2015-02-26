bf67664cc51deb6df5c97e26e0844a4d0208a7f4
Sam Gottfried <sgottfrd@gmail.com>
2015-02-26 18:59:19 -0500

    Added more view tests

--- test/views/search_view_tests.js
+++ test/views/search_view_tests.js
@@ -1,6 +1,9 @@
-var requests = [];
 var searchView;
 var testArea;
+
+var server;
+var xhr;
+var requests;
 
 describe('SearchView', function() {
   before(function() {
@@ -14,9 +17,10 @@ describe('SearchView', function() {
   });
 
   beforeEach(function() {
-    this.xhr = sinon.useFakeXMLHttpRequest();
     requests = [];
-    this.xhr.onCreate = function(xhr) {
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    xhr.onCreate = function(xhr) {
       requests.push(xhr);
     };
   });
@@ -24,13 +28,31 @@ describe('SearchView', function() {
   describe('#getResults', function() {
     it('should make a request to /search', function() {
       searchView.getResults('zelda');
+
       expect(requests.length).to.equal(1);
       expect(requests[0].url).to.equal('/search?query=zelda');
+    });
+
+    it('should render the result on the page', function() {
+      server = sinon.fakeServer.create();
+      server.respondWith("GET", "/search?query=zelda",
+        [
+          200,
+          {"Content-Type": "application/json"},
+          JSON.stringify({results: [{name: "The Legend of Zelda", image: { thumb_url: "http://static.giantbomb.com/uploads/scale_avatar/0/26/10169-legendofzelda-goldbox.png"}}]})
+        ]);
+      
+      searchView.getResults('zelda');
+      server.respond();
+
+      var results = document.getElementById('results');
+      expect(results.innerHTML).to.equal('<li>The Legend of Zelda<img src="http://static.giantbomb.com/uploads/scale_avatar/0/26/10169-legendofzelda-goldbox.png"></li>');
     });
   });
 
   after(function() {
-    this.xhr.restore();
+    xhr.restore();
+    server.restore;
     testArea.innerHTML = '';
   });
 });
