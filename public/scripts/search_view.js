a912178adeaee251d761e381c435a0b5f70182f2
Sam Gottfried <sgottfrd@gmail.com>
2015-02-26 17:59:50 -0500

    Removed requirejs

--- public/scripts/search_view.js
+++ public/scripts/search_view.js
@@ -1,32 +1,29 @@
-define(function() { 
+var SearchView = function() {
   var resultsBox = document.getElementById('results');
   var searchBar = document.getElementsByTagName('input')[0];
   var template = _.template(document.getElementById('gameTemplate').innerHTML);
 
   var searchTimeout;
+
+  this.getResults = function(query) {
+    resultsBox.innerHTML = '';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/search?query=' + query);
+    xhr.addEventListener('load', function() {
+      var results = JSON.parse(xhr.responseText);
+      results.results.forEach(function(r) {
+        resultsBox.innerHTML += template({name: r.name, image_url: r.image.thumb_url});
+      });
+    });
+
+    xhr.send();
+  }
+
+  var that = this;
   searchBar.addEventListener('input', function() {
     if (searchTimeout != undefined) {
       clearTimeout(searchTimeout);
     }
-    searchTimeout = setTimeout(function() { searchView.getResults(searchBar.value)}, 330);
+    searchTimeout = setTimeout(function() { that.getResults(searchBar.value)}, 330);
   });
-
-  searchView = {
-    getResults: function(query) {
-      resultsBox.innerHTML = '';
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', '/search?query=' + query);
-      xhr.addEventListener('load', function() {
-        var results = JSON.parse(xhr.responseText);
-        results.results.forEach(function(r) {
-          resultsBox.innerHTML += template({name: r.name, image_url: r.image.thumb_url});
-        });
-      });
-
-      xhr.send();
-    }
-  }
-
-  return searchView;
-});
-
+};
--- test/views/search_view_tests.js
+++ test/views/search_view_tests.js
@@ -1,36 +1,36 @@
-require.config({
-  paths: {
-    'searchView'    : '../../public/scripts/index',
-    'chai'          : 'vendor/javascripts/chai',
-  }
-});
+var requests = [];
+var searchView;
+var testArea;
 
-define(function(require) {
-  var chai = require('chai');
-  var expect = chai.expect;
-  var searchView = require('searchView');
+describe('SearchView', function() {
+  before(function() {
+    testArea = document.getElementById('testArea');
+    testArea.innerHTML = '<input /><ul id = "results"></ul>';
+    testArea.innerHTML += '<script type = "text/template" id = "gameTemplate">' +
+                          '<li><%= name %><img src = "<%= image_url %>"></li>' +
+                          '</script>';
 
-  var requests = [];
+    searchView = new SearchView();
+  });
 
-  describe('SearchView', function() {
-    beforeEach(function() {
-      this.xhr = sinon.useFakeXMLHttpRequest();
-      requests = [];
-      this.xhr.onCreate = function(xhr) {
-        requests.push(xhr);
-      };
+  beforeEach(function() {
+    this.xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    this.xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    };
+  });
+
+  describe('#getResults', function() {
+    it('should make a request to /search', function() {
+      searchView.getResults('zelda');
+      expect(requests.length).to.equal(1);
+      expect(requests[0].url).to.equal('/search?query=zelda');
     });
+  });
 
-    describe('#getResults', function() {
-      it('should make a request to /search', function() {
-        searchView.getResults('zelda');
-        expect(requests.length).to.equal(1);
-        expect(requests[0].url).to.equal('/search?query=zelda');
-      });
-    });
-
-    after(function() {
-      this.xhr.restore();
-    });
+  after(function() {
+    this.xhr.restore();
+    testArea.innerHTML = '';
   });
 });
