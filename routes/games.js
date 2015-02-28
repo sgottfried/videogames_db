068a6857f805b7e5639c1e1943d00dde65fcf961
Sam Gottfried <sgottfrd@gmail.com>
2015-02-28 12:14:08 -0500

    Added tests to add game route

--- routes/games.js
+++ routes/games.js
@@ -2,6 +2,7 @@ var models = require('../models');
 var Game = models.Game;
 
 var Routes = {
+  Games: Game,
   createGame: function(req, res) {
     var name = req.body.name;
     var giantBombApiId = req.body.giantBombApiId;
--- test/routes/games_test.js
+++ test/routes/games_test.js
@@ -1,30 +1,43 @@
-var models = require('../../models');
-var Game = models.Game;
 var gameRoutes = require('../../routes/games');
-var expect = require('chai').expect;
+
+var chai = require('chai');
+var expect = chai.expect;
 var sinon = require('sinon');
-var express = require('express');
+var when = require('when');
+
+var game, req;
 
 describe('Game Routes', function() {
   describe('POST games', function() {
+    before(function() {
+      game = {name: "the Legend of Zelda", giantBambApiId: '1'};
+      req = {body: {name: "the Legend of Zelda", giantBombApiId: '1'}};
+    });
+
     it('should persist a game', function(done) {
-      var req = {body: {name: "the Legend of Zelda", giantBombApiId: '1'}};
       var res = {json: function() {}};
       
       gameRoutes.createGame(req, res);
-      Game.count().then(function(count) {
+      gameRoutes.Games.count().then(function(count) {
         expect(count).to.equal(1);
         done();
       }).catch(function(err) {
         done(err);
       });
     });
+
+    it('should respond with json of the new post', function(done) {
+      var res = {json: function(gameJSON) {
+        expect(gameJSON).to.equal(game);
+        done();
+      }};
+
+      sinon.stub(gameRoutes.Games, 'create').returns(when(game));
+      gameRoutes.createGame(req, res);
+    });
   });
 
   afterEach(function() {
-    Game.destroy({where: {id: {$gt: 0}}});
+    gameRoutes.Games.destroy({where: {id: {$gt: 0}}});
   });
 });
-
-
-
