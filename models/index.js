ae624f9e0f4c6a584303297d5f6c9be5fbcb7664
Sam Gottfried <sgottfrd@gmail.com>
2016-02-28 12:33:27 -0500

    Updating config.json for heroku

--- models/index.js
+++ models/index.js
@@ -6,7 +6,18 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if(env === 'production') {
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true
+    },
+    logging:  true//false
+  });
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 var db        = {};
 
 fs
